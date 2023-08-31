1d8c8d27bcb0a832c2f04fb63ec8ca6eaf4fb4de
Victor de Souza Vilela da Silva <79282009+VictorVilelaSilva@users.noreply.github.com>
2023-08-30 21:52:56 -0300

    Update trabalho.js
    Inercia do carro e auto correcao do angulo da roda se tiver sem apertar nenhum botao

--- trabalho/trabalho.js
+++ trabalho/trabalho.js
@@ -1,28 +1,36 @@
-import * as THREE from  'three';
-import { OrbitControls } from '../build/jsm/controls/OrbitControls.js';
+import * as THREE from 'three';
+import {
+    OrbitControls
+} from '../build/jsm/controls/OrbitControls.js';
 import KeyboardState from '../libs/util/KeyboardState.js'
-import {initRenderer, 
-        initCamera,
-        initDefaultBasicLight,
-        setDefaultMaterial,
-        InfoBox,
-        onWindowResize,
-        createGroundPlaneXZ} from "../libs/util/util.js";
+import {
+    initRenderer,
+    initCamera,
+    initDefaultBasicLight,
+    setDefaultMaterial,
+    InfoBox,
+    onWindowResize,
+    createGroundPlaneXZ
+} from "../libs/util/util.js";
 
 let scene, renderer, camera, material, light, orbit; // Initial variables
-scene = new THREE.Scene();    // Create main scene
-renderer = initRenderer();    // Init a basic renderer
+scene = new THREE.Scene(); // Create main scene
+renderer = initRenderer(); // Init a basic renderer
 camera = initCamera(new THREE.Vector3(0, 15, 30)); // Init camera in this position
 material = setDefaultMaterial(); // create a basic material
 light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
-orbit = new OrbitControls( camera, renderer.domElement ); // Enable mouse rotation, pan, zoom etc.
+orbit = new OrbitControls(camera, renderer.domElement); // Enable mouse rotation, pan, zoom etc.
 
 // Listen window size changes
-window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+window.addEventListener('resize', function() {
+    onWindowResize(camera, renderer)
+}, false);
 
 // Show axes (parameter is size of each axis)
-let axesHelper = new THREE.AxesHelper( 12 );
-scene.add( axesHelper );
+let axesHelper = new THREE.AxesHelper(12);
+scene.add(axesHelper);
+var velocidade_carro = 0
+var acelerou = false;
 
 // To use the keyboard
 var keyboard = new KeyboardState();
@@ -37,13 +45,15 @@ scene.add(plane);
 //carroceria do carro
 let carroceriaGeometry = new THREE.BoxGeometry(6, 3, 5);
 let carroceria = new THREE.Mesh(carroceriaGeometry, material);
-carroceria.position.set(0.0, 2.0, 0.0);  // posicao da carroceria
+carroceria.position.set(0.0, 2.0, 0.0); // posicao da carroceria
 
 //farol da carroceria
-const geomeriaFarol = new THREE.ConeGeometry( 0.5, 0.5, 27 ); 
-const materialFarol = new THREE.MeshBasicMaterial( {color: 0x000000} );
-const Farol = new THREE.Mesh(geomeriaFarol, materialFarol );
-const Farol2 = new THREE.Mesh(geomeriaFarol, materialFarol );
+const geomeriaFarol = new THREE.ConeGeometry(0.5, 0.5, 27);
+const materialFarol = new THREE.MeshBasicMaterial({
+    color: 0x000000
+});
+const Farol = new THREE.Mesh(geomeriaFarol, materialFarol);
+const Farol2 = new THREE.Mesh(geomeriaFarol, materialFarol);
 
 Farol.position.set(3, -1, 1);
 Farol2.position.set(3, -1, -1);
@@ -51,13 +61,15 @@ Farol2.position.set(3, -1, -1);
 Farol.rotateZ(Graus_radianos(90))
 Farol2.rotateZ(Graus_radianos(90))
 
-carroceria.add( Farol );
-carroceria.add( Farol2 );
+carroceria.add(Farol);
+carroceria.add(Farol2);
 
 
 // rodas do carro
-let rodasGeometry = new THREE.CylinderGeometry( 1, 1, 1,64 );
-const materialRodas = new THREE.MeshBasicMaterial( {color: 0xffff00} ); 
+let rodasGeometry = new THREE.CylinderGeometry(1, 1, 1, 64);
+const materialRodas = new THREE.MeshBasicMaterial({
+    color: 0xffff00
+});
 let roda1 = new THREE.Mesh(rodasGeometry, materialRodas);
 let roda2 = new THREE.Mesh(rodasGeometry, materialRodas);
 let roda3 = new THREE.Mesh(rodasGeometry, materialRodas);
@@ -88,75 +100,114 @@ carroceria.rotateY(radianos);
 scene.add(carroceria);
 
 
+
+
 function keyboardUpdate() {
 
-  keyboard.update();
+    keyboard.update();
+    //angulo de rotacai do carro
+    var angle = THREE.MathUtils.degToRad(2);
+    var anguloRoda = 0;
+    console.log(velocidade_carro)
 
-  if ( keyboard.pressed("up") ){
-
-    carroceria.translateX(  .5 );
-
-  }
-  if ( keyboard.pressed("down") ){
-
-    carroceria.translateX( -.5 );
     
-  }  
+    
+    if (keyboard.pressed("up")) {
 
-  //angulo de rotacai do carro
-  var angle = THREE.MathUtils.degToRad(3);
-  var anguloRoda = 0;
+        velocidade_carro = 0.5
+        carroceria.translateX(velocidade_carro);
+        roda1.rotateZ(0);
+        acelerou = true;
 
-  
-  if ( keyboard.pressed("left") ){
-    //rotacao das rodas
-    if( roda1.rotation.z > Graus_radianos(-30)){
-      anguloRoda -= 0.01
-      roda1.rotateZ( anguloRoda );
-      roda3.rotateZ( anguloRoda );
-    }
-    if(keyboard.pressed("left")  && keyboard.pressed("up")) carroceria.rotateY( angle );
-
-    if(keyboard.pressed("left")  && keyboard.pressed("down")) carroceria.rotateY( -angle );
-  }  
-  if ( keyboard.pressed("right") ){
-    //rotacao das rodas
-      if(roda1.rotation.z < Graus_radianos(30)){
-        anguloRoda += 0.01
-        roda1.rotateZ( anguloRoda );
-        roda3.rotateZ( anguloRoda );
-        console.log(roda1.rotation.z);
-
+    }else{
+      if (velocidade_carro > 0 && acelerou) {
+        velocidade_carro -= 0.025
+        carroceria.translateX(velocidade_carro)
       }
-      if(keyboard.pressed("right")  && keyboard.pressed("up")) carroceria.rotateY( -angle );
+    } 
 
-      if(keyboard.pressed("right")  && keyboard.pressed("down")) carroceria.rotateY( angle );
-  } 
+    if (keyboard.pressed("down")) {
+        velocidade_carro = -0.5
+        carroceria.translateX(velocidade_carro);
+        acelerou = false;
 
+    }else{
+      if (velocidade_carro < 0 && !acelerou) {
+        velocidade_carro += 0.025
+        carroceria.translateX(velocidade_carro)
+      }
+    } 
+
+    if (keyboard.pressed("left")) {
+        //rotacao das rodas
+        if (roda1.rotation.z > Graus_radianos(-30)) {
+            anguloRoda -= Graus_radianos(1)
+            roda1.rotateZ(anguloRoda);
+            roda3.rotateZ(anguloRoda);
+        }
+        if (velocidade_carro > 0 && acelerou) {
+          carroceria.rotateY(angle)
+        }
+        if (keyboard.pressed("left") && keyboard.pressed("up")) carroceria.rotateY(angle);
+
+
+        if (keyboard.pressed("left") && keyboard.pressed("down")) carroceria.rotateY(-angle);
+    } else {
+        if (roda1.rotation.z < Graus_radianos(0)) {
+            anguloRoda += Graus_radianos(1)
+        
+            roda1.rotateZ(anguloRoda);
+            roda3.rotateZ(anguloRoda);
+        }
+    }
+    if (keyboard.pressed("right")) {
+        //rotacao das rodas
+        if (roda1.rotation.z < Graus_radianos(30)) {
+            anguloRoda += Graus_radianos(1)
+            roda1.rotateZ(anguloRoda);
+            roda3.rotateZ(anguloRoda);
+        }
+        if (velocidade_carro > 0 && acelerou) {
+          carroceria.rotateY(-angle)
+        }
+
+        if (keyboard.pressed("right") && keyboard.pressed("up")) carroceria.rotateY(-angle);
+
+        if (keyboard.pressed("right") && keyboard.pressed("down")) carroceria.rotateY(angle);
+    } else {
+        if (roda1.rotation.z > Graus_radianos(0)) {
+            anguloRoda -= Graus_radianos(1)
+          
+            roda1.rotateZ(anguloRoda);
+            roda3.rotateZ(anguloRoda)
+        }
+
+    }
 }
 
 // Use this to show information onscreen
 let controls = new InfoBox();
-  controls.add("Basic Scene");
-  controls.addParagraph();
-  controls.add("Use mouse to interact:");
-  controls.add("* Left button to rotate");
-  controls.add("* Right button to translate (pan)");
-  controls.add("* Scroll to zoom in/out.");
-  controls.show();
+controls.add("Basic Scene");
+controls.addParagraph();
+controls.add("Use mouse to interact:");
+controls.add("* Left button to rotate");
+controls.add("* Right button to translate (pan)");
+controls.add("* Scroll to zoom in/out.");
+controls.show();
 
 
 render();
 
 //funcao para tranformar graus em radianos
 function Graus_radianos(anguloGraus) {
-  var radianos = anguloGraus * (Math.PI / 180);
-  return radianos;
+    var radianos = anguloGraus * (Math.PI / 180);
+    return radianos;
 }
 
-function render()
-{
-  requestAnimationFrame(render);
-  keyboardUpdate();
-  renderer.render(scene, camera) // Render scene
+
+
+function render() {
+    requestAnimationFrame(render);
+    keyboardUpdate();
+    renderer.render(scene, camera) // Render scene
 }
