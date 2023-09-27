59878a3863b434cf08c39b975747a21c6f185da4
YuriFerreira03 <yurimoreiraferreira@hotmail.com>
2023-09-27 10:42:11 -0300

    l

--- trabalho/trabalho.js
+++ trabalho/trabalho.js
@@ -11,9 +11,10 @@ import {
   onWindowResize,
   createGroundPlaneXZ,
 } from "../libs/util/util.js";
+import { carroceria } from "./carro.js";
 import { Object3D } from "../build/three.module.js";
 
-let scene, renderer, camera, material, light, orbit; // Initial variables
+let scene, renderer, camera, light, orbit; // Initial variables
 scene = new THREE.Scene(); // Create main scene
 renderer = initRenderer(); // Init a basic renderer
 camera = initCamera(new THREE.Vector3(0, 15, 30)); // Init camera in this position
@@ -23,7 +24,6 @@ let cameraOffset = new THREE.Vector3(-30, 25, 40);
 let cameraLookAhead = 5.0;
 let modoInspecao = true;
 let pista = new THREE.Object3D();
-var radianos = Graus_radianos(90);
 let PistaEscolhida = 0;
 
 // contador de voltas
@@ -111,82 +111,6 @@ let clock = new THREE.Clock();
 let plane = createGroundPlaneXZ(20, 20);
 scene.add(plane);
 
-//carroceria do carro
-let carroceriaGeometry = new THREE.BoxGeometry(6, 1.5, 3);
-material = new THREE.MeshPhongMaterial({ color: 0xff0000 }); // create a basic material
-let carroceria = new THREE.Mesh(carroceriaGeometry, material);
-carroceria.position.set(0.0, 1.0, 0.0); // posicao da carroceria
-orbit.target.set(carroceria.position.x, carroceria.position.y,carroceria.position.z);
-
-//farol da carroceria
-const geomeriaFarol = new THREE.ConeGeometry(0.4, 0.25, 17);
-const materialFarol = new THREE.MeshBasicMaterial({
-  color: 0x000000,
-});
-const Farol = new THREE.Mesh(geomeriaFarol, materialFarol);
-const Farol2 = new THREE.Mesh(geomeriaFarol, materialFarol);
-
-Farol.position.set(3, -0.5, 1);
-Farol2.position.set(3, -0.5, -1);
-
-Farol.rotateZ(Graus_radianos(90));
-Farol2.rotateZ(Graus_radianos(90));
-
-carroceria.add(Farol);
-carroceria.add(Farol2);
-
-// aquilo
-let AROgeometry = new THREE.CapsuleGeometry(0.1, 0.6, 4, 8);
-let AROmaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-let ARO = new THREE.Mesh(AROgeometry, AROmaterial);
-let ARO2 = new THREE.Mesh(AROgeometry, AROmaterial);
-let ARO3 = new THREE.Mesh(AROgeometry, AROmaterial);
-let ARO4 = new THREE.Mesh(AROgeometry, AROmaterial);
-
-ARO.position.set(0, 0.2, 0);
-ARO2.position.set(0, 0.2, 0);
-ARO3.position.set(0, -0.2, 0);
-ARO4.position.set(0, -0.2, 0);
-
-ARO.rotateX(radianos);
-ARO2.rotateX(radianos);
-ARO3.rotateX(radianos);
-ARO4.rotateX(radianos);
-
-// rodas do carro
-let rodasGeometry = new THREE.CylinderGeometry(0.5, 0.5, 0.5, 32);
-const materialRodas = new THREE.MeshBasicMaterial({
-  color: 0x00000,
-});
-let roda1 = new THREE.Mesh(rodasGeometry, materialRodas);
-let roda2 = new THREE.Mesh(rodasGeometry, materialRodas);
-let roda3 = new THREE.Mesh(rodasGeometry, materialRodas);
-let roda4 = new THREE.Mesh(rodasGeometry, materialRodas);
-
-roda1.position.set(2.3, -0.6, 1.7);
-roda2.position.set(-2.3, -0.6, 1.7);
-roda3.position.set(2.3, -0.6, -1.7);
-roda4.position.set(-2.3, -0.6, -1.7);
-
-//rotaciona a roda em 90 graus
-
-roda1.rotateX(radianos);
-roda2.rotateX(radianos);
-roda3.rotateX(radianos);
-roda4.rotateX(radianos);
-
-//adiciona as rodas na carroceria
-roda1.add(ARO);
-roda2.add(ARO2);
-roda3.add(ARO3);
-roda4.add(ARO4);
-carroceria.add(roda1);
-carroceria.add(roda2);
-carroceria.add(roda3);
-carroceria.add(roda4);
-carroceria.rotateY(radianos);
-
-// add the carroceria to the scene
 scene.add(carroceria);
 
 function updateCameraPosition() {
