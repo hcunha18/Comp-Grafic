b46e3c44c63f3f7480657e1ea6a4b9bdfb82b20c
Victor de Souza Vilela da Silva <79282009+VictorVilelaSilva@users.noreply.github.com>
2023-11-25 12:52:53 -0300

    Feat:adicionando skybox

--- trabalho2/trabalho.js
+++ trabalho2/trabalho.js
@@ -255,6 +255,32 @@ virtualCamera.position.copy(camPosition);
 virtualCamera.rotation.set(-1.5708, 0, 0);
 virtualCamera.up.copy(upVec);
 
+
+////////////skybox///////////
+var faceArray = [];
+    var textureFront = new THREE.TextureLoader().load( 'skybox/bun_ft.jpg');
+    var textureBack = new THREE.TextureLoader().load( 'skybox/bun_bk.jpg');
+    var textureTop = new THREE.TextureLoader().load( 'skybox/bun_up.jpg');
+    var textureBottom = new THREE.TextureLoader().load( 'skybox/bun_dn.jpg');
+    var textureRight = new THREE.TextureLoader().load( 'skybox/bun_rt.jpg');
+    var textureLeft = new THREE.TextureLoader().load( 'skybox/bun_lf.jpg');
+      
+    faceArray.push(new THREE.MeshBasicMaterial( { map: textureFront }));
+    faceArray.push(new THREE.MeshBasicMaterial( { map: textureBack }));
+    faceArray.push(new THREE.MeshBasicMaterial( { map: textureTop }));
+    faceArray.push(new THREE.MeshBasicMaterial( { map: textureBottom }));
+    faceArray.push(new THREE.MeshBasicMaterial( { map: textureRight }));
+    faceArray.push(new THREE.MeshBasicMaterial( { map: textureLeft }));
+       
+    for (var i = 0; i < 6; i++)
+      faceArray[i].side = THREE.BackSide;
+       
+    var skyboxGeometry = new THREE.BoxGeometry( 1000, 1000, 1000);
+    var skybox = new THREE.Mesh( skyboxGeometry, faceArray );
+    // skybox.rotateX(degreesToRadians(90));
+    scene.add( skybox );
+////////////////////
+
 function keyboardUpdate(position) {
   keyboard.update();
   //angulo de rotacai do carro
